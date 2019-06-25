a3aabb7dfad13b9f275d1d52f815a4238dddbe04
Vladislav <plachey1@gmail.com>
2019-06-25 16:17:45 +0300

    2 commit

--- users_postgres/api/models.py
+++ users_postgres/api/models.py
@@ -1,16 +1,18 @@
-from api.manage import db, ma, bcrypt
+from manage import db, ma, bcrypt
 from sqlalchemy.dialects.postgresql import UUID
 from sqlalchemy import text
 import datetime
+# from uuid import uuid4
 
 
 class Users(db.Model):
     id = db.Column(UUID(as_uuid=True), primary_key=True, server_default=text("uuid_generate_v4()"))
-    username = db.Column(db.String(64), index=True, unique=True)
-    email = db.Column(db.String(120), index=True, unique=True)
+    # id = db.Column(db.Text, primary_key=True, unique=True, default=uuid4())
+    username = db.Column(db.String(64), unique=True)
+    email = db.Column(db.String(120), unique=True)
     password_hash = db.Column(db.String(128))
-    address = db.Column(db.String(200))
-    date = db.Column(db.DateTime, default=datetime.datetime.utcnow())
+    user_address = db.Column(db.String(200))
+    create_user_date = db.Column(db.DateTime, default=datetime.datetime.utcnow())
 
     def __repr__(self):
         return '<User {}'.format(self.username)
@@ -24,7 +26,7 @@ class Users(db.Model):
 
 class UsersSchema(ma.Schema):
     class Meta:
-        fields = ('id', 'username', 'email', 'password_hash', 'address', 'date')
+        fields = ('id', 'username', 'email', 'password_hash', 'user_address', 'create_user_date')
 
 
 user_schema = UsersSchema()
